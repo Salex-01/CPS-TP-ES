3aee63ccbeb261cc26c9bb9564a04b7272f43148
Salex <salex@orange.fr>
2019-04-04 14:30:04 +0200

    #7 : normalement ça marche

--- bit_operations.c
+++ bit_operations.c
@@ -9,9 +9,12 @@ BFILE* bstart(FILE* f, const char* mode){
 	}
 	BFILE* B = malloc(sizeof(BFILE));
 	B->f = f;
-	B->decal = 8;
+	B->decal = 0;
 	B->mode = malloc((strlen(mode)+1)*sizeof(char));
 	strcpy(B.mode,mode);
+	if((mode[0]==r)||(mode[1]==r)){
+		fread(bf->buff,8,1,f);
+	}
 	return B;
 }
 
@@ -19,41 +22,47 @@ int bstop(BFILE* bf){
 	if(bf==NULL){
 		return 0;
 	}
-	if((bf->decal!=8)&&((bf->mode[0]=='w')||(bf->mode[1]=='w'))){
-		while(bf->decal<8){
-			bitwrite(bf,0);
-		}
+	while(bf->decal!=8){
+		bitwrite(bf,0);
 	}
+	fwrite(bf->buff,8,1,bf->f);
 	free(bf->mode);
 	free(bf);
 	return 0;
 }
 
 char bitread(BFILE *bf){
-	char b = fgetc(bf->f);
-	if(b == EOF){
+	if(bf->f==NULL){
 		return -1;
 	}
-	if(bf->decal < 7){
-		fseek(bf->f, -1, SEEK_CUR);
-	}
-	else{
+	if(bf->decal==8){
+		fwrite(bf->buff,8,1,bf->f);
+		fread(bf->buff,8,1,bf->f);
 		bf->decal = 0;
 	}
-	return (b >> 7 - bf->decal) & 1;
+	if(bf->buff == EOF){
+		return -1;
+	}
+	bf->decal++;
+	return ((bf->buff >> (8-bf->decal)) & 1);
 }
 
 int bitwrite(BFILE* bf, char bit){
-    if(bf->f == NULL)
-        return -1;
-    bf->buff &= (0 << 7-bf->decal);
-	bf->buff |= (bit & 1) << 7-bf->decal;
-    bf->decal++;
-    if(bf->decal == 7){
-        fputc(bf->f, bf->buff);
-        bf->decal = 0;
-    }
-    return 0;
+	if(bf->f == NULL){
+		return -1;
+	}
+	if(bf->decal==8){
+		fwrite(bf->buff,8,1,bf->f);
+		bf->decal=0;
+	}
+	if(bit){
+		bf->buff |= (1<<(7-decal));
+	}
+	else{
+		bf->buff &= ~(1<<(7-decal));
+	}
+	bf->decal++;
+	return 0;
 }
 
 int beof(BFILE* bf){
